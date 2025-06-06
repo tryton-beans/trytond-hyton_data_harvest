6690eea4c0e103ddff261a5f56878827f9bde124
francesc <francesc.guillen@gmail.com>
2025-06-06 18:35:45 +0200

    Small corrections. Check params. Create form for config

--- __init__.py
+++ __init__.py
@@ -1,12 +1,13 @@
 import hy
 from trytond.pool import Pool
-from . import query, result, execute
+from . import query, result, execute, config
 
 def register():
     Pool.register(
         query.HarvestQuery,
         result.HarvestResult,
         execute.HarvestExecuteQueryStart,
+        config.HarvestConfig,
         module='hyton_data_harvest', type_='model')
     Pool.register(
         execute.HarvestExecuteQueryWizard,
